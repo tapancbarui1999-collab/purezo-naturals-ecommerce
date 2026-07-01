4b48dd404b13eebeb7957c528c8ae7d5501dbf3b
tapancbarui1999-collab <tapancbarui1999@gmail.com>
2026-07-01 21:57:22 +0530

    Create main.jsx

---
+++ client/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+
+/**
+ * Entry point for PUREZO NATURALS E-commerce Platform.
+ * Configured with React Router for SPA navigation.
+ */
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
